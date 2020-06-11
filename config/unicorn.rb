ba4309bf4b99bd86ad61528dc1c6bfb7f56c17be
FumiakiTono <tonotorious@gmail.com>
2020-06-12 01:41:20 +0900

    second commit

---
+++ config/unicorn.rb
@@ -0,0 +1,43 @@
+rails_root = File.expand_path('../../', __FILE__)
+
+worker_processes 2
+
+working_directory rails_root
+
+timeout 300
+listen 8080
+
+pid '/var/run/unicorn.pid'
+#listen '/share/unicorn.sock'
+
+stdout_path "./log/unicorn.stdout.log"
+stderr_path "./log/unicorn.stderr.log"
+
+pid File.expand_path('../../tmp/pids/unicorn.pid', __FILE__)
+
+# 基本的には`true`を指定する。Unicornの再起動時にダウンタイムなしで再起動が行われる。
+preload_app true
+# 効果なしとの記事を見たので、コメントアウト。
+# GC.respond_to?(:copy_on_write_friendly=) and GC.copy_on_write_friendly = true
+
+# USR2シグナルを受けると古いプロセスを止める。
+# 後述するが、記述しておくとNginxと連携する時に良いことがある。
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.establish_connection
+end
+
+
